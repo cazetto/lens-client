ab7aa41f05dd9918ec0a4d931d94201b4ea615bb
André Pesci Cazetto <cazetto.andre@gmail.com>
2019-05-15 08:43:46 -0300

    Added onsenui and react-onsenui

--- src/global.css.js
+++ src/global.css.js
@@ -1,6 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
 import { accent } from 'constants/theme';
 
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
+
 export default createGlobalStyle`
   html, body, div, span, applet, object, iframe,
   h1, h2, h3, h4, h5, h6, p, blockquote, pre,
