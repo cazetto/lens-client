9520e5d3c87294094058a77a657623e711e21faa
André Pesci Cazetto <cazetto.andre@gmail.com>
2019-05-17 15:49:35 -0300

    Using rebassjs

--- src/components/search-list-item/search-list-item.js
+++ src/components/search-list-item/search-list-item.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { Box, Card, Heading, Text } from 'rebass';
+
 const SearchListItem = props => (
-  <>
-    <p>{props.data.title}</p>
-    <p>{props.data.description}</p>
-    <p>{props.data.content}</p>
-  </>
+  <Card
+    mb={20}
+    pb={20}
+    pt={20}
+    px={10}
+    borderRadius={3}
+    bg="light-blue"
+    boxShadow="1px 1px 6px rgba(0, 0, 0, .04)"
+  >
+    <Box px={2}>
+      <Heading as="h3">{props.data.title}</Heading>
+      <Text as="p" fontSize={0}>
+        {props.data.description}
+      </Text>
+      <Text as="p" fontSize={2}>
+        {props.data.content}
+      </Text>
+    </Box>
+  </Card>
 );
 
 SearchListItem.propTypes = {
